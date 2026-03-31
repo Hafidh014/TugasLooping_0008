365e7af26b45bdb3a74fd477a978a80605533c69
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-03-31 20:48:19 +0700

    cek bilangan prima

--- 1.cpp
+++ 1.cpp
@@ -5,3 +5,14 @@ using namespace std;
 
 int angka;
 int pilihan;
+
+bool isPrima(int n) {
+    if (n <= 1) return false;
+
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
